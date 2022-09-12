78bc01db28d0f64c57df624a4192e17bfa9ac448
Oliver Grawert <ogra@ubuntu.com>
2022-09-12 23:30:26 +0000

    fix vidcapture.py window handling

--- demos/bin/vidcapture.py
+++ demos/bin/vidcapture.py
@@ -14,7 +14,7 @@ while(True):
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('frame',gray)
+    cv2.imshow('Video',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
